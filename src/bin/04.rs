ca90613a2a79772893cc12646009ca91d4b9d5d3
Bradley Nelson <bradley@nel.family>
2024-12-04 23:52:27 -0700

    day 4 part 2

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -47,6 +47,41 @@ fn check_position(list: Vec<Vec<char>>, i: usize, j: usize) -> i32 {
     count
 }
 
+const CORNERS: [(i32, i32, &str); 4] = [
+    (1, 1, "down right"), // down right
+    (1, -1, "down left"), // down left
+    (-1, -1, "up left"), // up left
+    (-1, 1, "up right") // up right
+];
+
+fn check_position2(list: Vec<Vec<char>>, i: i32, j: i32) -> bool {
+    let mut count = 0;
+    for corner in CORNERS.iter() {
+        let x: i32 = corner.0.try_into().unwrap();
+        let y: i32 = corner.1.try_into().unwrap();
+        let index1 = i + x;
+        let index2 = j + y;
+        if index1 < 0 || index2 < 0 || index1 as usize >= list.len() || index2 as usize >= list[0].len() {
+            continue;
+        }
+        let char = list[index1 as usize][index2 as usize];
+        match char {
+            'M' => {
+                let index1 = i + (-1*x);
+                let index2 = j + (-1*y);
+                if index1 < 0 || index2 < 0 || index1 as usize >= list.len() || index2 as usize >= list[0].len() {
+                    continue;
+                }
+                if list[index1 as usize][index2 as usize] == 'S' {
+                    count += 1;
+                }
+            }
+            _ => {}
+        }
+    }
+    count == 2
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
     let processed_input = parse_input(input);
     let mut count = 0;
@@ -61,7 +96,18 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let processed_input = parse_input(input);
+    let mut count = 0;
+    for line in processed_input.iter().enumerate() {
+        for (j, c) in line.1.iter().enumerate() {
+            if *c == 'A' {
+                if check_position2(processed_input.clone(), line.0 as i32, j as i32) {
+                    count += 1;
+                }
+            }
+        }
+    }
+    Some(count.try_into().unwrap())
 }
 
 #[cfg(test)]
@@ -77,6 +123,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(9));
     }
 }
